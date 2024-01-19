7fe8c988dd4f8e0de5547b3b1b8cc4cd8f6de596
janhavipatil03 <112199711+janhavipatil03@users.noreply.github.com>
2024-01-20 00:00:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// Clearing display screen
+function clear() {
+  document.getElementById("result").value = "";
+}
+// Appending next character
+function append(char) {
+  document.getElementById("result").value += char;
+}
+// Calculation
+function calculate() {
+  let x = document.getElementById("result").value;
+  let y = eval(x);
+  document.getElementById("result").value = y;
+  if(y == "Infinity"){
+    alert("Invalid Operation");
+    location.reload();
+    return;
+  }
+}
+// Deleting last character
+function del() {
+  let x = document.getElementById("result").value;
+  y = x.slice(0, -1);
+  document.getElementById("result").value = y;
+}
